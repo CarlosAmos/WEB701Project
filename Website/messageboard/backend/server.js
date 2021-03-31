aaceb8f12d3802edf9a8433ec7d2d465b3250a4f
CarlosAmos <carlos.amos1@gmail.com>
2021-03-31 17:11:37 +1300

    Updated Server.js

--- Website/messageboard/backend/server.js
+++ Website/messageboard/backend/server.js
@@ -1,16 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
 const port =  3000;
+const url = 'mongodb://localhost:27017';
+const dbName = 'SuppliesForSchools';
+const db;
 
 app.use(bodyParser.text());
 app.use(cors());
 
 app.post('/api/message', (req,res) => {
     console.log(req.body);
+    db.collection('messages').insertOne({'msg': req.body});
     res.status(200).send();
+})
+
+MongoClient.connect(url, function (err, client) {
+
+    if(err) return console.log('mongodb error', err);
+
+    console.log("connected successfully to server");
+
+    db = client.db(dbName);
+    client.close();
 })
 
 app.listen(port, () => console.log('App running on port', port));
